f6a5d193d2e606fc86d595f6d7ca1f3f84573592
OlusanyaAyomide <101556220+OlusanyaAyomide@users.noreply.github.com>
2024-01-04 14:26:57 +0000

    regex-fix

--- components/ListClient.tsx
+++ components/ListClient.tsx
@@ -6,8 +6,7 @@ export default function ListClient({children}:{children:React.ReactNode}){
     const path = usePathname()
     const regex = /\/hostel\/([^/]+)$/;
     const match = path.match(regex);
-    const slug = match[1] || null
-    console.log(slug)
+    const slug = match?match[1] || null:null
     const isMe = slug === "all-entry"
     return (
         <div className="mt-4">
